ff414222f492d809b10416aa96f7118c40a89091
Tommy Kärnström <tommy.karnstrom@hotmail.com>
2019-01-26 23:28:08 +0100

    added nvm to source

--- source.sh
+++ source.sh
@@ -4,5 +4,5 @@
 # Make sure everything in on one line
 
 VISUAL_STUDIO_CODE_EXTENSIONS='aaron-bond.better-comments CoenraadS.bracket-pair-colorizer-2 DavidAnson.vscode-markdownlint dbaeumer.vscode-eslint eamodio.gitlens formulahendry.auto-close-tag formulahendry.auto-rename-tag ms-vscode.atom-keybindings msjsdiag.debugger-for-chrome ritwickdey.LiveServer vincaslt.highlight-matching-tag'
-BREW_FORMULAS='bash bash-completion git git-flow-avh watchman'
+BREW_FORMULAS='bash bash-completion git git-flow-avh watchman nvm'
 BREW_CASKS='android-studio appcleaner dash docker firefox google-chrome iterm2 mamp postman prepros slack spotify the-unarchiver typora visual-studio-code vlc'
