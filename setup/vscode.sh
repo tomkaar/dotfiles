5e3c22696ea6a66d2f8905d5ef1eeea0411ef10a
Tommy Kärnström <tommy.karnstrom@hotmail.com>
2019-03-15 14:38:54 +0100

    added setup and status for vscode settings

--- setup/vscode.sh
+++ setup/vscode.sh
@@ -7,8 +7,18 @@ source $DIR/utils/vscode.sh
 
 info "🛠  Visual Studio Code setup" A
 
+CONFIG_SRC="${DIR}/dotfiles/vscode/settings.json"
+CONFIG_DEST="${HOME}/Library/Application Support/Code/User/settings.json"
+
 install_vscode_prompt
+
 vscode_install_extensions
+
+text_space_above "Settings"
+if find_file_location "$CONFIG_SRC" "vscode/settings"; then
+    createLink "$CONFIG_SRC" "$CONFIG_DEST"
+    symlink_location_matches "$CONFIG_DEST" "$CONFIG_SRC" 
+fi
 
 complete_message Y
 
--- status/vscode.sh
+++ status/vscode.sh
@@ -5,11 +5,19 @@ DIR="$1"
 source $DIR/utils/utils.sh
 source $DIR/utils/vscode.sh
 
+CONFIG_SRC="${DIR}/dotfiles/vscode/settings.json"
+CONFIG_DEST="${HOME}/Library/Application Support/Code/User/settings.json"
+
 info "⚙️  ${TEXT_BOLD}Visual Studio Code status${TEXT_NORMAL}" Y
 text_space_below "This will check if the 'code' command can be used\nand which extensions are installed"
 
 if command_exists code; then
     success "  ✓  Visual Studio Code is installed"
+
+    text_space_above 'Settings'
+    find_file_location_only_visual "$CONFIG_SRC" "vscode/settings source"
+    find_file_location_only_visual "$CONFIG_DEST" "vscode/settings destionation"
+    symlink_location_matches "$CONFIG_DEST" "$CONFIG_SRC" 
 
     text_space 'Installed Extensions, matching source file'
     vscode_installed_extensions_list
