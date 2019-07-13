19608a9d74e6f718c170f064b4f4bc9aae840856
Tommy Kärnström <tommy.karnstrom@hotmail.com>
2019-07-13 21:56:33 +0200

    updated vscode extensions

--- source.sh
+++ source.sh
@@ -3,6 +3,6 @@
 # This is the source of truth
 # Make sure everything in on one line
 
-VISUAL_STUDIO_CODE_EXTENSIONS='aaron-bond.better-comments CoenraadS.bracket-pair-colorizer-2 DavidAnson.vscode-markdownlint dbaeumer.vscode-eslint eamodio.gitlens formulahendry.auto-close-tag formulahendry.auto-rename-tag ms-vscode.atom-keybindings msjsdiag.debugger-for-chrome ritwickdey.LiveServer vincaslt.highlight-matching-tag artdiniz.quitcontrol-vscode davidbabel.vscode-simpler-icons'
+VISUAL_STUDIO_CODE_EXTENSIONS='aaron-bond.better-comments CoenraadS.bracket-pair-colorizer-2 DavidAnson.vscode-markdownlint dbaeumer.vscode-eslint formulahendry.auto-close-tag formulahendry.auto-rename-tag ms-vscode.atom-keybindings msjsdiag.debugger-for-chrome vincaslt.highlight-matching-tag artdiniz.quitcontrol-vscode davidbabel.vscode-simpler-icons'
 BREW_FORMULAS='bash bash-completion git git-flow-avh watchman nvm'
 BREW_CASKS='android-studio appcleaner dash docker firefox google-chrome iterm2 mamp postman prepros slack spotify the-unarchiver typora visual-studio-code vlc now'
