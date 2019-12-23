72602f5603c274f1ba2e00bb99168b7ff3f7bed6
Tommy Kärnström <tommy.karnstrom@hotmail.com>
2019-12-23 01:33:40 +0100

    Added directory creation check to ssh setup

--- src/ssh/setup.sh
+++ src/ssh/setup.sh
@@ -7,6 +7,13 @@ CONFIG_SRC="${DIR}/dotfiles/ssh/config"
 CONFIG_DEST="${HOME}/.ssh/config"
 
 if find_file_location "$CONFIG_SRC" ".ssh/config"; then
+    # check if directory exists
+    if [ ! -d "${HOME}/.ssh" ]; then
+        if ask "Could not find directory ${TEXT_BOLD}${HOME}/.ssh${TEXT_NORMAL}, do you want to created it?" Y; then
+            echo "Creating directory ${TEXT_BOLD}${HOME}/.ssh${TEXT_NORMAL}"
+            mkdir "${HOME}/.ssh"
+        fi
+    fi
     createLink "$CONFIG_SRC" "$CONFIG_DEST"
     symlink_location_matches "$CONFIG_DEST" "$CONFIG_SRC" 
 fi
