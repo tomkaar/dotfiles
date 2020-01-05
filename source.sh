de2400f8cbd889556f889436ef8ddcf53dc1af02
Tommy Kärnström <tommy.karnstrom@hotmail.com>
2020-01-05 01:03:46 +0100

    added cheatsheet to BREW_CASKS source

--- source.sh
+++ source.sh
@@ -5,5 +5,5 @@
 
 VISUAL_STUDIO_CODE_EXTENSIONS='aaron-bond.better-comments CoenraadS.bracket-pair-colorizer-2 DavidAnson.vscode-markdownlint dbaeumer.vscode-eslint formulahendry.auto-close-tag formulahendry.auto-rename-tag ms-vscode.atom-keybindings msjsdiag.debugger-for-chrome vincaslt.highlight-matching-tag artdiniz.quitcontrol-vscode davidbabel.vscode-simpler-icons'
 BREW_FORMULAS='bash bash-completion git git-flow-avh watchman'
-BREW_CASKS='appcleaner dash docker firefox google-chrome iterm2 mamp nightowl postman slack spotify the-unarchiver typora visual-studio-code vlc now'
+BREW_CASKS='appcleaner cheatsheet dash docker firefox google-chrome iterm2 mamp nightowl postman slack spotify the-unarchiver typora visual-studio-code vlc now'
 GLOBAL_NPM_PACKAGES='diff-so-fancy'
