bb4776a8fcf13752473d92b41d067c91cea500b3
Tommy Kärnström <tommy.karnstrom@hotmail.com>
2020-01-30 19:50:00 +0100

    added Git History and GitLens to source

--- source.sh
+++ source.sh
@@ -3,7 +3,7 @@
 # This is the source of truth
 # Make sure everything in on one line
 
-VISUAL_STUDIO_CODE_EXTENSIONS='aaron-bond.better-comments CoenraadS.bracket-pair-colorizer-2 DavidAnson.vscode-markdownlint dbaeumer.vscode-eslint formulahendry.auto-close-tag formulahendry.auto-rename-tag ms-vscode.atom-keybindings msjsdiag.debugger-for-chrome vincaslt.highlight-matching-tag artdiniz.quitcontrol-vscode davidbabel.vscode-simpler-icons'
+VISUAL_STUDIO_CODE_EXTENSIONS='aaron-bond.better-comments CoenraadS.bracket-pair-colorizer-2 DavidAnson.vscode-markdownlint dbaeumer.vscode-eslint formulahendry.auto-close-tag formulahendry.auto-rename-tag ms-vscode.atom-keybindings msjsdiag.debugger-for-chrome vincaslt.highlight-matching-tag artdiniz.quitcontrol-vscode davidbabel.vscode-simpler-icons donjayamanne.githistory eamodio.gitlens'
 BREW_FORMULAS='bash bash-completion git git-flow-avh watchman'
 BREW_CASKS='appcleaner cheatsheet dash docker firefox google-chrome iterm2 mamp nightowl postman slack spotify the-unarchiver typora visual-studio-code vlc now'
 GLOBAL_NPM_PACKAGES='diff-so-fancy'
