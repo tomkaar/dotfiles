fc2b376564356a971e24921325c5e5e841598640
Tommy Kärnström <tommy.karnstrom@hotmail.com>
2022-04-10 14:48:27 +0200

    feat(source): updated list of packages

--- source.sh
+++ source.sh
@@ -1,9 +1,3 @@
-
-
-# This is the source of truth
-# Make sure everything in on one line
-
-VISUAL_STUDIO_CODE_EXTENSIONS='aaron-bond.better-comments CoenraadS.bracket-pair-colorizer-2 DavidAnson.vscode-markdownlint dbaeumer.vscode-eslint formulahendry.auto-close-tag formulahendry.auto-rename-tag ms-vscode.atom-keybindings msjsdiag.debugger-for-chrome vincaslt.highlight-matching-tag artdiniz.quitcontrol-vscode davidbabel.vscode-simpler-icons donjayamanne.githistory eamodio.gitlens'
-BREW_FORMULAS='bash bash-completion git git-flow-avh watchman'
-BREW_CASKS='appcleaner cheatsheet dash docker firefox google-chrome iterm2 mamp nightowl postman slack spotify the-unarchiver typora visual-studio-code vlc now'
+VISUAL_STUDIO_CODE_EXTENSIONS='aaron-bond.better-comments DavidAnson.vscode-markdownlint dbaeumer.vscode-eslint formulahendry.auto-close-tag formulahendry.auto-rename-tag ms-vscode.atom-keybindings artdiniz.quitcontrol-vscode davidbabel.vscode-simpler-icons atomiks.moonlight donjayamanne.githistory eamodio.gitlens ms-vsliveshare.vsliveshare'
+BREW_FORMULAS='iterm2 git bash bash-completion watchman cheatsheet docker nightowl postman obsidian brave-browser discord spotify the-unarchiver visual-studio-code messenger'
 GLOBAL_NPM_PACKAGES='diff-so-fancy'
